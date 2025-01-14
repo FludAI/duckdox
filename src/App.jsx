8e0edf2a95541d5b602ea039891aad21e684cf40
FludAI <129318711+FludAI@users.noreply.github.com>
2025-01-14 15:24:14 -0800

    To include zip file that makes it easy to deploy on Amplify with GUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
-import './globals.css'
-import RaceTrack from './components/RaceTrack'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './globals.css';
+import RaceTrack from './components/RaceTrack';
+import BirthdayParadoxGame from './components/BirthdayParadoxGame';
 
 function App() {
   return (
-    <div className="min-h-screen bg-background">
-      <main className="container mx-auto py-10">
-        <RaceTrack />
-      </main>
-    </div>
-  )
+    <Router>
+      <div className="min-h-screen bg-background">
+        {/* Main Container */}
+        <main className="container mx-auto py-10">
+          {/* Routes */}
+          <Routes>
+            <Route path="/" element={<RaceTrack />} />
+            <Route path="/presidential-experiment" element={<BirthdayParadoxGame />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
